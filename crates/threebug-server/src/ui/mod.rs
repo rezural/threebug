b4866215da0d597da8951586066cde12f5e00daf
rezural <rezural@protonmail.com>
2021-12-16 16:11:29 +1100

    show sessions as selectable grid

--- crates/threebug-server/src/ui/mod.rs
+++ crates/threebug-server/src/ui/mod.rs
@@ -1,36 +1,35 @@
-use bevy::prelude::ResMut;
+use bevy::{prelude::*, window::Windows};
 use bevy_egui::EguiContext;
 
 use crate::resource::session::Sessions;
 
-pub fn ui(mut ctx: ResMut<EguiContext>, mut sessions: ResMut<Sessions>) {
-    debug_sessions::sessions(&mut *ctx, &mut *sessions);
+pub fn ui(mut ctx: ResMut<EguiContext>, mut sessions: ResMut<Sessions>, windows: Res<Windows>) {
+    let window = windows.get_primary().unwrap();
+    debug_sessions::sessions(&mut *ctx, &mut *sessions, window);
 }
 
 pub mod debug_sessions {
-    use bevy::prelude::info;
-    // use bevy::prelude::info;
+    use bevy::window::Window;
     use bevy_egui::{
-        egui::{self, ComboBox},
+        egui::{self, Grid, ScrollArea},
         EguiContext,
     };
 
     use crate::resource::session::Sessions;
 
-    pub fn sessions(ctx: &mut EguiContext, sessions: &mut Sessions) {
+    pub fn sessions(ctx: &mut EguiContext, sessions: &mut Sessions, window: &Window) {
         egui::Window::new("Sessions").show(ctx.ctx(), |ui| {
             let session_ids = sessions.session_ids();
             if let Some(current) = sessions.current_session_id_mut() {
-                ComboBox::from_label("Choose Session")
-                    .selected_text(current.clone())
-                    .show_ui(ui, |ui| {
-                        info!("current_session: {}", current);
+                let height = window.height() * 0.8;
+                ScrollArea::vertical().max_height(height).show(ui, |ui| {
+                    Grid::new("Sessions").show(ui, |ui| {
                         for session in session_ids {
-                            info!("session: {}", session);
                             ui.selectable_value(current, session.clone(), session);
-                            // ui.selectable_label(current, session.clone(), session);
+                            ui.end_row();
                         }
                     });
+                })
             }
         });
     }
