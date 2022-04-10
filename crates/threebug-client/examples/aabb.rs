aab2a3f3346ddc675179524a7f062261175732ac
rezural <rezural@protonmail.com>
2022-04-11 09:37:48 +1000

    add random radius cmd line switch to example client

--- crates/threebug-client/examples/aabb.rs
+++ crates/threebug-client/examples/aabb.rs
@@ -13,10 +13,12 @@ struct Options {
     count: usize,
     #[structopt(short, long, default_value = "10")]
     volume_radius: f32,
-    #[structopt(short, long, default_value = "0.5")]
+    #[structopt(short, long, default_value = "2.0")]
     aabb_radius: f32,
     #[structopt(short, long, default_value = "0.5")]
     wait: f32,
+    #[structopt(short, long)]
+    random_radius: bool,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -46,8 +48,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         let vrange = -opt.volume_radius..opt.volume_radius;
         let centre = Point3::origin().map(|_: f32| rng.gen_range(vrange.clone()));
 
-        let mins = centre.map(|c| c - opt.aabb_radius);
-        let maxs = centre.map(|c| c + opt.aabb_radius);
+        let radius = if opt.random_radius {
+            opt.aabb_radius
+        } else {
+            rng.gen::<f32>() * opt.aabb_radius
+        };
+        let mins = centre.map(|c| c - radius);
+        let maxs = centre.map(|c| c + radius);
 
         let aabb = parry3d::bounding_volume::AABB::new(mins, maxs);
 
